7af39ec5486bad74259eeb7a03fe6f1262e02151
Nahuel Morena <nahuel1morena1@gmail.com>
2023-06-12 23:11:14 -0300

    get user data to view profile (simple design)

--- backend/src/main/java/natbra/appstarter/server/controllers/UserController.java
+++ backend/src/main/java/natbra/appstarter/server/controllers/UserController.java
@@ -1,6 +1,7 @@
 package natbra.appstarter.server.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import natbra.appstarter.server.model.auth.UserType;
 import natbra.appstarter.server.repository.UserTypeRepository;
@@ -29,6 +30,11 @@ private final String baseUrl = "/admin/users";
 	@GetMapping(baseUrl)
 	public HttpEntity<List<User>> getUsers(){
 		return ResponseEntity.ok(userRepository.findAll());
+	}
+
+	@GetMapping(baseUrl + "/get-user")
+	public HttpEntity<Optional<User>> getUser(){
+		return ResponseEntity.ok(userRepository.findById(utils.getAuthUser().getId()));
 	}
 
 	@GetMapping(baseUrl + "/get-players")
--- frontend/src/views/ProfileView/index.vue
+++ frontend/src/views/ProfileView/index.vue
@@ -1,5 +1,40 @@
 <template>
-    <div class="about">
-        <h1>This is an about page</h1>
+    <div>
+        <h1>Perfil del Usuario</h1>
+        <div v-if="user">
+            <p>Nombre: {{ user.name }}</p>
+            <p>Apellido: {{ user.surname }}</p>
+            <p>Email: {{ user.email }}</p>
+            <p>Teléfono: {{ user.telephone }}</p>
+            <p>Fecha de Nacimiento: {{ user.birthdate }}</p>
+            <p>Tipo: {{ user.usertype }}</p>
+            <!-- Otros datos del usuario -->
+        </div>
+        <div v-else>
+            <p>Cargando...</p>
+        </div>
     </div>
 </template>
+
+<script>
+import { localAxios } from "@/axios";
+import { SnackbarStore } from "@/store/snackbar";
+
+export default {
+    data() {
+        return {
+            snackbarStore: SnackbarStore(),
+            user: null,
+        };
+    },
+    async mounted() {
+        try {
+            const response = await localAxios.get("/admin/users/get-user"); // Reemplaza "1" por el ID del usuario que deseas obtener
+            this.user = response.data;
+            console.log(this.user);
+        } catch (error) {
+            console.error(error);
+        }
+    },
+};
+</script>
