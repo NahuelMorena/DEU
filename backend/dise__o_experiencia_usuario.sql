324194bb234564de32affc10c4af476ffabdd5f0
Nahuel Morena <nahuel1morena1@gmail.com>
2023-07-29 14:28:32 -0300

    update data database

--- backend/dise__o_experiencia_usuario.sql
+++ backend/dise__o_experiencia_usuario.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-06-2023 a las 22:06:39
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.0.19
+-- Tiempo de generación: 29-07-2023 a las 19:23:22
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `authorities` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `authorities`
@@ -49,7 +49,9 @@ INSERT INTO `authorities` (`id`, `name`) VALUES
 (15, 'CAN_VIEW_EDIT_PLANIFICATION'),
 (16, 'CAN_VIEW_USERSPLANIFICATIONS'),
 (17, 'CAN_VIEW_PROFILE'),
-(18, 'CAN_VIEW_SETTING');
+(18, 'CAN_VIEW_SETTING'),
+(19, 'CAN_VIEW_PLAYERSREQUESTS'),
+(20, 'CAN_VIEW_PLANIFICATIONBYPLAYER');
 
 -- --------------------------------------------------------
 
@@ -61,7 +63,7 @@ CREATE TABLE `authorities_roles` (
   `id` int(11) NOT NULL,
   `id_authority` int(11) NOT NULL,
   `id_role` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `authorities_roles`
@@ -90,7 +92,23 @@ INSERT INTO `authorities_roles` (`id`, `id_authority`, `id_role`) VALUES
 (44, 18, 1),
 (45, 17, 2),
 (46, 17, 3),
-(47, 17, 1);
+(47, 17, 1),
+(48, 19, 3),
+(49, 20, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `califications`
+--
+
+CREATE TABLE `califications` (
+  `id` int(11) NOT NULL,
+  `note` tinyint(2) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_training` int(11) NOT NULL,
+  `id_trainer_planification` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -113,7 +131,7 @@ CREATE TABLE `databasechangelog` (
   `CONTEXTS` varchar(255) DEFAULT NULL,
   `LABELS` varchar(255) DEFAULT NULL,
   `DEPLOYMENT_ID` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `databasechangelog`
@@ -164,7 +182,7 @@ CREATE TABLE `databasechangeloglock` (
   `LOCKED` bit(1) NOT NULL,
   `LOCKGRANTED` datetime DEFAULT NULL,
   `LOCKEDBY` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `databasechangeloglock`
@@ -182,17 +200,17 @@ INSERT INTO `databasechangeloglock` (`ID`, `LOCKED`, `LOCKGRANTED`, `LOCKEDBY`) 
 CREATE TABLE `planifications` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `planifications`
 --
 
 INSERT INTO `planifications` (`id`, `name`) VALUES
-(8, 'Probando'),
-(22, 'ahora si gato'),
-(23, 'ASDA'),
-(24, 'nuevo');
+(28, 'Entrenamientos de Arquero'),
+(29, 'Entrenamientos de Delanteros'),
+(30, 'Entrenamientos de Mediocampistas'),
+(31, 'Entrenamientos de Defensas');
 
 -- --------------------------------------------------------
 
@@ -203,7 +221,7 @@ INSERT INTO `planifications` (`id`, `name`) VALUES
 CREATE TABLE `roles` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `roles`
@@ -226,18 +244,23 @@ CREATE TABLE `trainer_planifications` (
   `id_planification` int(11) NOT NULL,
   `order_number` tinyint(4) NOT NULL,
   `minutes` tinyint(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `trainer_planifications`
 --
 
 INSERT INTO `trainer_planifications` (`id`, `id_training`, `id_planification`, `order_number`, `minutes`) VALUES
-(10, 17, 22, 0, 5),
-(17, 17, 23, 0, 5),
-(21, 16, 23, 2, 71),
-(22, 17, 23, 1, 10),
-(23, 16, 24, 0, 50);
+(32, 24, 28, 2, 20),
+(33, 24, 28, 0, 20),
+(34, 23, 28, 1, 10),
+(35, 22, 29, 1, 20),
+(36, 21, 29, 0, 20),
+(37, 20, 30, 2, 20),
+(38, 19, 30, 1, 10),
+(39, 19, 30, 3, 10),
+(40, 23, 31, 0, 15),
+(41, 20, 31, 1, 15);
 
 -- --------------------------------------------------------
 
@@ -254,16 +277,19 @@ CREATE TABLE `trainings` (
   `description` text NOT NULL,
   `warmup_time` int(11) NOT NULL,
   `repetitions_quantity` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `trainings`
 --
 
 INSERT INTO `trainings` (`id`, `name`, `date`, `id_user`, `id_training_type`, `description`, `warmup_time`, `repetitions_quantity`) VALUES
-(15, 'asd', '2023-06-04', 1, 2, 'cvxcvxvcx', 34, 34),
-(16, 'nuevo', '2023-06-05', 7, 2, 'asd', 4, 3),
-(17, 'Otro entrenamiento', '2023-06-05', 7, 3, 'Nuevo entrenamiento', 4, 33);
+(19, 'Precisión en el pase', '2023-07-29', 7, 2, 'Mejora la precisión y la velocidad en los pases cortos y largos.', 10, 4),
+(20, 'Dominio y control del balón', '2023-07-29', 7, 3, 'Trabajar en el primer toque y el control de balón en diferentes situaciones.', 10, 10),
+(21, 'Tiro a la portería', '2023-07-29', 7, 4, 'Practicar diferentes tipos de disparos para mejorar la técnica de finalización.', 4, 5),
+(22, 'Regate y cambio de dirección', '2023-07-29', 7, 2, 'Mejorar las habilidades de regate y cambios rápidos de dirección.', 3, 5),
+(23, 'Defensa individual', '2023-07-29', 7, 3, 'Trabajar en la técnica de marcaje y la anticipación defensiva.', 10, 2),
+(24, 'Reflejo y reacción', '2023-07-29', 7, 2, 'Ejercicios específicos para desarrollar reflejos y rapidez de reacción en los porteros.', 10, 3);
 
 -- --------------------------------------------------------
 
@@ -274,7 +300,7 @@ INSERT INTO `trainings` (`id`, `name`, `date`, `id_user`, `id_training_type`, `d
 CREATE TABLE `training_type` (
   `id` int(11) NOT NULL,
   `name` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `training_type`
@@ -303,7 +329,7 @@ CREATE TABLE `users` (
   `birthdate` date NOT NULL,
   `telephone` int(30) NOT NULL,
   `id_user_type` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `users`
@@ -311,11 +337,14 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `id_trainer`, `username`, `password`, `name`, `surname`, `email`, `birthdate`, `telephone`, `id_user_type`) VALUES
 (1, NULL, 'admin', '$2a$12$BngXzPDbcSen1b.TCNha6uszCLVTiGoXCfo35q34KyP7bk5MSA/Km', 'Josefina', 'Gonzalez', 'admin@gmail.com', '1991-08-15', 23424235, NULL),
-(3, 7, 'juan', '$2a$12$BngXzPDbcSen1b.TCNha6uszCLVTiGoXCfo35q34KyP7bk5MSA/Km', 'Juan', 'Ramirez', 'juan@gmail.com', '1990-01-16', 5435435, 1),
+(3, 7, 'juan', '$2a$12$BngXzPDbcSen1b.TCNha6uszCLVTiGoXCfo35q34KyP7bk5MSA/Km', 'Juan', 'Ramirez', 'juan@gmail.com', '1990-01-15', 5435435, 1),
 (5, NULL, 'admin2', '$2a$12$BngXzPDbcSen1b.TCNha6uszCLVTiGoXCfo35q34KyP7bk5MSA/Km', 'Jorge', 'Gonzalez', 'admin@gmail.com', '1991-08-15', 23424235, NULL),
 (7, NULL, 'fer', '$2a$10$mXA2/0VX76NmiF3.7pkY3eY.wmnSEv6.qjwAA4Q/X7buKFXGz9vAm', 'Fermin', 'Moreno', 'ferminmorenoz99@gmail.com', '1991-05-14', 1341412421, NULL),
 (8, 7, 'pedro', '$2a$12$BngXzPDbcSen1b.TCNha6uszCLVTiGoXCfo35q34KyP7bk5MSA/Km', 'Pedro', 'Gonzalez', 'pedro@gmail.com', '1990-01-17', 5435435, 2),
-(9, 7, 'victor', '$2a$12$BngXzPDbcSen1b.TCNha6uszCLVTiGoXCfo35q34KyP7bk5MSA/Km', 'Victor', 'Romero', 'victor@gmail.com', '1990-01-16', 5435435, 3);
+(9, 7, 'victor', '$2a$12$BngXzPDbcSen1b.TCNha6uszCLVTiGoXCfo35q34KyP7bk5MSA/Km', 'Victor', 'Romero', 'victor@gmail.com', '1990-01-16', 5435435, 3),
+(14, 7, 'pablo', '$2a$10$mnU2pfar5ESvsyXqljeWO.2iRoumNoHFm4lQQ0KE6RRyll1ZitEsi', 'Pablo', 'Perez', 'pablo@gmail.com', '2023-06-13', 345345, 2),
+(15, 7, 'juanm', '$2a$10$G2Oiu/2.osMOp.0CW1KpKe3ccm6ZlFz09zeElR6jncFZbSSj6Mhlm', 'Juan', 'Martinez', 'juanm@gmail.com', '1978-05-09', 43533453, 2),
+(16, 7, 'player', '$2a$10$wxcZSNAmvBv4aSLsNUTvfOSzo4zPr7kG1u3mkoiQNYqwk0QRpEz4u', 'Aaron ', 'Molina', 'player@gmail.com', '2003-05-06', 221345623, 1);
 
 -- --------------------------------------------------------
 
@@ -328,22 +357,18 @@ CREATE TABLE `user_planifications` (
   `id_user` int(11) NOT NULL,
   `id_planification` int(11) NOT NULL,
   `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `user_planifications`
 --
 
 INSERT INTO `user_planifications` (`id`, `id_user`, `id_planification`, `date`) VALUES
-(2, 3, 8, '2023-06-06'),
-(3, 8, 8, '2023-06-06'),
-(4, 9, 8, '2023-06-06'),
-(5, 3, 23, '2023-06-07'),
-(6, 8, 23, '2023-06-07'),
-(7, 9, 23, '2023-06-07'),
-(8, 3, 22, '2023-06-20'),
-(10, 9, 22, '2023-06-20'),
-(11, 9, 24, '2023-06-22');
+(15, 16, 31, '2023-07-31'),
+(16, 8, 29, '2023-07-31'),
+(17, 14, 29, '2023-07-31'),
+(18, 3, 31, '2023-07-31'),
+(19, 9, 30, '2023-07-31');
 
 -- --------------------------------------------------------
 
@@ -355,19 +380,20 @@ CREATE TABLE `user_roles` (
   `id` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `id_role` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `user_roles`
 --
 
 INSERT INTO `user_roles` (`id`, `id_user`, `id_role`) VALUES
-(1, 1, 1),
 (2, 1, 2),
 (6, 7, 3),
-(10, 3, 1),
 (12, 8, 1),
-(14, 9, 1);
+(14, 9, 1),
+(19, 3, 1),
+(21, 14, 1),
+(22, 16, 1);
 
 -- --------------------------------------------------------
 
@@ -378,7 +404,7 @@ INSERT INTO `user_roles` (`id`, `id_user`, `id_role`) VALUES
 CREATE TABLE `user_type` (
   `id` int(11) NOT NULL,
   `name` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `user_type`
@@ -406,6 +432,15 @@ ALTER TABLE `authorities_roles`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_authority` (`id_authority`),
   ADD KEY `id_role` (`id_role`);
+
+--
+-- Indices de la tabla `califications`
+--
+ALTER TABLE `califications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user_calificated` (`id_user`),
+  ADD KEY `id_training` (`id_training`),
+  ADD KEY `id_trainer_planification` (`id_trainer_planification`);
 
 --
 -- Indices de la tabla `databasechangeloglock`
@@ -485,19 +520,25 @@ ALTER TABLE `user_type`
 -- AUTO_INCREMENT de la tabla `authorities`
 --
 ALTER TABLE `authorities`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `authorities_roles`
 --
 ALTER TABLE `authorities_roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+
+--
+-- AUTO_INCREMENT de la tabla `califications`
+--
+ALTER TABLE `califications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT de la tabla `planifications`
 --
 ALTER TABLE `planifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
@@ -509,13 +550,13 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `trainer_planifications`
 --
 ALTER TABLE `trainer_planifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT de la tabla `trainings`
 --
 ALTER TABLE `trainings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `training_type`
@@ -527,19 +568,19 @@ ALTER TABLE `training_type`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de la tabla `user_planifications`
 --
 ALTER TABLE `user_planifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `user_roles`
 --
 ALTER TABLE `user_roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `user_type`
@@ -557,6 +598,14 @@ ALTER TABLE `user_type`
 ALTER TABLE `authorities_roles`
   ADD CONSTRAINT `authority_role` FOREIGN KEY (`id_authority`) REFERENCES `authorities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `role_authority` FOREIGN KEY (`id_role`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `califications`
+--
+ALTER TABLE `califications`
+  ADD CONSTRAINT `id_trainer_planification` FOREIGN KEY (`id_trainer_planification`) REFERENCES `trainer_planifications` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_training` FOREIGN KEY (`id_training`) REFERENCES `trainings` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_user_calificated` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `trainer_planifications`
